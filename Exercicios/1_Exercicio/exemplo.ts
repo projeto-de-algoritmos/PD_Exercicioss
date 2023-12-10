52aef67a6f0f8c9738517ae1dd4100ca363ec3ec
valdersonjr <valdersonpontes@gmail.com>
2023-12-10 20:38:49 -0300

    exercicio 1

---
+++ Exercicios/1_Exercicio/exemplo.ts
@@ -0,0 +1,10 @@
+import lengthOfLIS from "./maiorSubsequenciaCrescente";
+
+const nums1 = [10, 9, 2, 5, 3, 7, 101, 18];
+console.log(lengthOfLIS(nums1)); // Saída: 4
+
+const nums2 = [0, 1, 0, 3, 2, 3];
+console.log(lengthOfLIS(nums2)); // Saída: 4
+
+const nums3 = [7, 7, 7, 7, 7, 7, 7];
+console.log(lengthOfLIS(nums3)); // Saída: 1
